93a372b9a6e9eedbd83f013c7d39d316c55408bb
Raymond Chu <raychu@berkeley.edu>
2017-02-21 18:52:37 -0800

    damn.

--- TicTacToePonzi.js
+++ TicTacToePonzi.js
@@ -2,9 +2,9 @@ pragma solidity ^0.4.9;
 
 contract TicTacToePonzi {
 
-	adress owner;
+    adress owner;
 
-	 struct Player {
+     struct Player {
         address addr;
 
         uint256 position; 
@@ -25,13 +25,13 @@ contract TicTacToePonzi {
 
 
     function TicTacToePonzi() {
-    	potBalance = 0;
-    	owner = msg.sender;
+        potBalance = 0;
+        owner = msg.sender;
 
     }
 
     function getPlayer(address addr) private {
-    	for(int i = 0; i < players.length; i++) {
+        for(int i = 0; i < players.length; i++) {
             if(players[i].addr == addr) {
                 return players[i];
             }
@@ -40,63 +40,67 @@ contract TicTacToePonzi {
     }
 
     function getBuyInThreshold() returns (uint256){
-    	return buyInThreshold;
+        return buyInThreshold;
     }
 
     function joinGame() payable {
-    	potBalance += msg.value;
-    	players.push(Player(msg.sender, msg.value, 0)); //not sure what this does
-    	Player player = getPlayer(msg.origin);
-    	if(msg.value > buyInThreshold){
-    		//then do stuff
-    		if(players.length == 1){
-    			player.position = 1;
-    		}
-    		else{
-    			player.position = 0;
-    		
-
-    			//PLAY GAME HERE
-
-    			gameNum++;
-    		}
+        potBalance += msg.value;
+        players.push(Player(msg.sender, msg.value, 0)); //not sure what this does
+        Player player = getPlayer(msg.origin);
+        if(msg.value >= 1.1 * buyInThreshold){  //Challenger commits 1.1x buyIn by default. Refunded later if he wins and chooses not to increase
+            //then do stuff
+            if(players.length == 1){
+                player.position = 1;
+            }
+            else{
+                player.position = 0;
+            
+ 
+                //PLAY GAME HERE
 
 
-    	}
-    	else{
-    		player.deposit += msg.value;
-    	}
 
-    	player.totalOwned = player.deposit + player.value;
+                //challenger chooses how much money to input (minimum 1.1 if he loses, 1.0 if he wins), gets refund if msg.value > chosen value
+                gameNum++;
+            }
+
+
+        }
+        else{
+            player.deposit += msg.value;
+        }
+
+        player.totalOwned = player.deposit + player.value;
 
     }
 
     function withdrawFunds() public {
-    	Player player = getPlayer(msg.origin);
-    	if(player.addr == msg.origin){
-    		uint256 accountBalance = player.totalOwned;
-    		player.deposit = 0;
-    		player.value = 0;
-    		player.totalOwned = 0;
-    		if (!(player.addr.deposit.value(accountBalance)())) {
-            	    throw;
-            	}
-		}
+        Player player = getPlayer(msg.origin);
+        if(player.addr == msg.origin){
+            uint256 accountBalance = player.totalOwned;
+            player.deposit = 0;
+            player.value = 0;
+            player.totalOwned = 0;
+            if (!(player.addr.deposit.value(accountBalance)())) {
+                    throw;
+                }
+        }
     }
 
 
     function payeeWinsOrDraws() payable {
 
-    	//change player.value and player.totalOwned of payee
-    	//change player.canWithdraw to true for payee
-    	//Challenger becommes new payee with value of however much he paid
+        //change player.value and player.totalOwned of payee
+        //change player.canWithdraw to true for payee
+        //Challenger becommes new payee with value of however much he paid
 
     }
 
     function challengerWins() payable {
 
-    	//Payee values stay same, but canWithdraw becomes true.
-    	//Challenger becomes payee with value of however much he decides to pay
+        //Payee values stay same, but canWithdraw becomes true.
+        //Challenger becomes payee with value of however much he decides to pay
+
 
     }
 
