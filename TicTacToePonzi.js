e7d72f1e69ba0be633aa6d71da3cb13c008c63d9
Raymond Chu <Raymond Chu>
2017-02-25 22:58:45 -0800

    added a bunch of conditions

--- TicTacToePonzi.js
+++ TicTacToePonzi.js
@@ -8,26 +8,28 @@ contract TicTacToePonzi {
         address addr;
 
         uint256 position; 
-        //1 for Payee and 0 for Challenger. -1 for not in game, 2 for in queue
+        //1 for Challenger and 0 for Payee. 2 for queue 
         
-        uint256 value; //Amount of money in pot from winnings. Becomes 0 after witdrawl
+        uint256 value; //Amount of money in pot from winnings. Becomes 0 after witdrawal
         uint256 deposit; //If doesn't meet threshold
 
-        uint256 totalOwned; //Value + Deposit. How much money owned in the contract
+        uint256 totalOwned; //Value + Deposit + investment. How much money owned in the contract
 
         bool canWithdraw;
 
         bool playing; //Currently in a game
+
 
     }
 
     struct GameStatus{
         bool inGame; //Is there a game going on?
 
-        Player[] pl; //Current Players
+        Player[] currentPlayers; //Current Players
+
+        Player[] queue;
 
         //Maybe variable for board state?
-
 
     }
 
@@ -39,23 +41,57 @@ contract TicTacToePonzi {
 
 
     function TicTacToePonzi(uint256 investment) {
-        uint256 pos = 0;
-        
-        if(players.length == 1){
-            pos = 1;
+        uint256 pos ;
+        if(players.length == 0 || players.length == 1){  //Owner of the contract (person who deployed)
+            pos = players.length;
+        }    
+        else if( players.length > 1){
+            pos = 2;
         }
+        
+        Player temp = Player(msg.sender, pos,  0, 0, investment, false, false);
 
-        
-        
-        players.push(Player(msg.sender, pos,  investment, 0, investment, false, false));
+        players.push(temp);
+
+
+        if(pos == 0 || pos == 1){
+            pl[pos] = temp;
+        }
+        else{
+            queue.push(temp);
+        }
 
         //DEPOSIT THE MONEY INTO POT, TAKE MONEY FROM WALLET (how do we do this?)
 
     }
 
+    function getQueuePos() public returns (int) {
+        Player temp = Player[getPlayerAddr(msg.sender)];
+        int i = 0;
+        if(queue.length == 0){
+            return 0;
+        }
+        for(; i < queue.length; i++){
+            if(temp == players[i]){
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
     function getTime() public {
 
         //calls win if blocks or time past an hour
+    }
+
+    function updateQueue(Player[] arr, int loc) private {
+        if(arr.length <= 0 || loc >= arr.length){
+            return;
+        }
+        for(int i = loc; i < arr.length-1; i++){
+            arr[i] = arr[i+1];
+        }
+        delete arr[i+1];
     }
 
 
@@ -74,8 +110,15 @@ contract TicTacToePonzi {
     function playGame() public payable {
 
         //check if 2 players are in gameStatus and both are not in a game already and no other game is going on 
-
+        if(inGame || currentPlayers.length != 2 || currentPlayers[0] == currentPlayers[1] || currentPlayers[0].playing || currentPlayers[1].playing){
+            throw;
+        }
         
+
+        ingame = true;
+        currentPlayers[0].playing = false;
+        currentPlayers[1].playing = false;
+
         potBalance += msg.value;
          
         Player player = players[getPlayerAddr(msg.sender)];
@@ -101,19 +144,67 @@ contract TicTacToePonzi {
 
         //player.totalOwned = player.deposit + player.value;
 
+
+
+
+        //Game ends: Queue updated, game statuses updated, current players updated.
+        currentPlayers[0].playing = false;
+        currentPlayers[1].playing = false;
+        currentPlayers[0].canWithdraw = true;
+        inGame = false;
+        currentPlayers[0] = currentPlayers[1];
+        if(queue.length == 0){
+            delete currentPlayers[1];
+        }
+        else{
+            currentPlayers[1] == queue[0];
+            updateQueue(queue, 0);
+        }
+
+
     }
 
     function withdrawFunds() public {
         Player player = players[getPlayerAddr(msg.sender)];
         if(player.addr == msg.sender){
-            uint256 accountBalance = player.totalOwned;
+            player.totalOwned += player.deposit;
+            player.totalOwned += player.value;
             player.deposit = 0;
             player.value = 0;
-            player.totalOwned = 0;
             // if (!(player.addr.deposit.value(accountBalance)())) {
             //         throw;
             //     }
         }
+    }
+
+    function leaveGame() public {
+        //Allows you to leave the game only if player is not in a game and is not a payee.
+
+        Player player = players[getPlayerAddr(msg.sender)];
+        if(player.playing){
+            return;
+        }
+
+        if(player.pos == 0 && pl[0] == player){  //payees cant leave 
+            return;
+        }
+        else if(player.pos == 1 && pl[1] == player){
+
+            if(queue.length == 0){
+                delete currentPlayers[1];
+            }
+            else{
+                currentPlayers[1] == queue[0];
+                updateQueue(queue, 0);
+            }
+
+        }
+        else if(player.pos == 2){
+            updateQueue(queue, player.getQueuePos());
+        }
+
+        //Send all funds back to wallet somehow
+
     }
 
 
